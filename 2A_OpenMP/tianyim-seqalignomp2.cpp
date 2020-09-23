401f2fac86b907e840d0584711584059583315fc
motianyi <motianyi99@gmail.com>
2020-09-23 11:54:22 +0800

    improve 2a

--- 2A_OpenMP/tianyim-seqalignomp2.cpp
+++ 2A_OpenMP/tianyim-seqalignomp2.cpp
@@ -8,73 +8,77 @@
 
 using namespace std;
 
-int getMinimumPenalty(std::string x, std::string y, int pxy, int pgap, int *xans, int *yans);
+int getMinimumPenalty(std::string x, std::string y, int pxy, int pgap, int* xans, int* yans);
 
 // Return current time, for performance measurement
 uint64_t GetTimeStamp() {
     struct timeval tv;
-    gettimeofday(&tv, NULL);
-    return tv.tv_sec * (uint64_t) 1000000 + tv.tv_usec;
+    gettimeofday(&tv,NULL);
+    return tv.tv_sec*(uint64_t)1000000+tv.tv_usec;
 }
 
 
 // Driver code
-int main() {
-    int misMatchPenalty;
-    int gapPenalty;
-    std::string gene1;
-    std::string gene2;
-    std::cin >> misMatchPenalty;
-    std::cin >> gapPenalty;
-    std::cin >> gene1;
-    std::cin >> gene2;
-    std::cout << "misMatchPenalty=" << misMatchPenalty << std::endl;
-    std::cout << "gapPenalty=" << gapPenalty << std::endl;
+int main(){
+	int misMatchPenalty;
+	int gapPenalty;
+	std::string gene1;
+	std::string gene2;
+	std::cin >> misMatchPenalty;
+	std::cin >> gapPenalty;
+	std::cin >> gene1;
+	std::cin >> gene2;
+	std::cout << "misMatchPenalty=" << misMatchPenalty << std::endl;
+	std::cout << "gapPenalty=" << gapPenalty << std::endl;
 
-    int m = gene1.length(); // length of gene1
-    int n = gene2.length(); // length of gene2
-    int l = m + n;
-    int xans[l + 1], yans[l + 1];
+	int m = gene1.length(); // length of gene1
+	int n = gene2.length(); // length of gene2
+	int l = m+n;
+	int xans[l+1], yans[l+1];
 
-    uint64_t start = GetTimeStamp();
+	uint64_t start = GetTimeStamp ();
 
-    // calling the function to calculate the result
-    int penalty = getMinimumPenalty(gene1, gene2,
-                                    misMatchPenalty, gapPenalty,
-                                    xans, yans);
+	// calling the function to calculate the result
+	int penalty = getMinimumPenalty(gene1, gene2,
+		misMatchPenalty, gapPenalty,
+		xans,yans);
+	
+	// print the time taken to do the computation
+	printf("Time: %ld us\n", (uint64_t) (GetTimeStamp() - start));
+	
+	// postprocessing of the answer, for printing results
 
-    // print the time taken to do the computation
-    printf("Time: %ld us\n", (uint64_t)(GetTimeStamp() - start));
+	// Since we have assumed the answer to be n+m long,
+	// we need to remove the extra gaps in the starting
+	// id represents the index from which the arrays
+	// xans, yans are useful
+	int id = 1;
+	int i;
+	for (i = l; i >= 1; i--)
+	{
+		if ((char)yans[i] == '_' && (char)xans[i] == '_')
+		{
+			id = i + 1;
+			break;
+		}
+	}
+	
+	// Printing the final answer
+	std::cout << "Minimum Penalty in aligning the genes = ";
+	std::cout << penalty << std::endl;
+	std::cout << "The aligned genes are :" << std::endl;
+	for (i = id; i <= l; i++)
+	{
+		std::cout<<(char)xans[i];
+	}
+	std::cout << "\n";
+	for (i = id; i <= l; i++)
+	{
+		std::cout << (char)yans[i];
+	}
+	std::cout << "\n";
 
-    // postprocessing of the answer, for printing results
-
-    // Since we have assumed the answer to be n+m long,
-    // we need to remove the extra gaps in the starting
-    // id represents the index from which the arrays
-    // xans, yans are useful
-    int id = 1;
-    int i;
-    for (i = l; i >= 1; i--) {
-        if ((char) yans[i] == '_' && (char) xans[i] == '_') {
-            id = i + 1;
-            break;
-        }
-    }
-
-    // Printing the final answer
-    std::cout << "Minimum Penalty in aligning the genes = ";
-    std::cout << penalty << std::endl;
-    std::cout << "The aligned genes are :" << std::endl;
-    for (i = id; i <= l; i++) {
-        std::cout << (char) xans[i];
-    }
-    std::cout << "\n";
-    for (i = id; i <= l; i++) {
-        std::cout << (char) yans[i];
-    }
-    std::cout << "\n";
-
-    return 0;
+	return 0;
 }
 
 /******************************************************************************/
@@ -84,71 +88,67 @@ int main() {
 #include "omp.h"
 #include "math.h"
 
-inline int min3(int a, int b, int c) {
-    if (a <= b) {
-        if (a <= c) {
-            return a;
-        }
-        return c;
-    } else if (b <= c) {
-        return b;
-    }
-    return c;
-}
+// inline int min3(int a, int b, int c) {
+// 	if (a <= b && a <= c) {
+// 		return a;
+// 	} else if (b <= a && b <= c) {
+// 		return b;
+// 	} else {
+// 		return c;
+// 	}
+// }
 
 // equivalent of  int *dp[width] = new int[height][width]
 // but works for width not known at compile time.
 // (Delete structure by  delete[] dp[0]; delete[] dp;)
-inline int **new2d(int width, int height) {
-    int **dp = new int *[width];
-    size_t size = width;
-    size *= height;
-    int *dp0 = new int[size];
-    if (!dp || !dp0) {
-        std::cerr << "getMinimumPenalty: new failed" << std::endl;
-        exit(1);
-    }
-    dp[0] = dp0;
+int **new2d (int width, int height)
+{
+	int **dp = new int *[width];
+	size_t size = width;
+	size *= height;
+	int *dp0 = new int [size];
+	if (!dp || !dp0)
+	{
+	    std::cerr << "getMinimumPenalty: new failed" << std::endl;
+	    exit(1);
+	}
+	dp[0] = dp0;
+	for (int i = 1; i < width; i++)
+	    dp[i] = dp[i-1] + height;
 
-
-    for (int i = 1; i < width; i++)
-        dp[i] = dp[i - 1] + height;
-
-    return dp;
+	return dp;
 }
 
 // function to find out the minimum penalty
 // return the maximum penalty and put the aligned sequences in xans and yans
 int getMinimumPenalty(std::string x, std::string y, int pxy, int pgap,
-                      int *xans, int *yans) {
-    int i, j; // intialising variables
+	int* xans, int* yans)
+{
+	int i, j; // intialising variables
+	
+	int m = x.length(); // length of gene1
+	int n = y.length(); // length of gene2
+	
+	// table for storing optimal substructure answers
+	int **dp = new2d (m+1, n+1);
+//	size_t size = m + 1;
+//	size *= n + 1;
+//	memset (dp[0], 0, size);
 
-    int m = x.length(); // length of gene1
-    int n = y.length(); // length of gene2
+	// intialising the table
+	#pragma omp parallel for
+	for (i = 0; i <= m; i++)
+	{
+		dp[i][0] = i * pgap;
+	}
+	#pragma omp parallel for
+	for (i = 0; i <= n; i++)
+	{
+		dp[0][i] = i * pgap;
+	}
 
-    // table for storing optimal substructure answers
-    omp_set_num_threads(omp_get_max_threads());
-    int **dp = new2d(m + 1, n + 1);
-
-
-    // remove unnecessary memset
-//    size_t size = m + 1;
-//    size *= n + 1;
-//    memset (dp[0], 0, size);
-
-    // intialising the table
-#pragma omp parallel for
-    for (i = 0; i <= m; ++i) {
-        dp[i][0] = i * pgap;
-    }
-#pragma omp parallel for
-    for (i = 0; i <= n; ++i) {
-        dp[0][i] = i * pgap;
-    }
-
-    
 	int number_threads = 22;
-
+    omp_set_num_threads(number_threads);
 	//integer devision
 	//calculate the deminsion of each block
 	int block_width = m / number_threads;
@@ -161,7 +161,7 @@ int getMinimumPenalty(std::string x, std::string y, int pxy, int pgap,
 		int column_min = max(0, k - number_threads + 1);
         int column_max = min(k, number_threads - 1);
 
-		#pragma omp parallel for shared(dp, x,y)
+		#pragma omp parallel for
 		for (int column = column_min; column <= column_max; column++){
 			int row = k - column;
 
@@ -188,8 +188,8 @@ int getMinimumPenalty(std::string x, std::string y, int pxy, int pgap,
 						if (x[i - 1] == y[j - 1]){
 							dp[i][j] = dp[i - 1][j - 1];
 						}else{
-							dp[i][j] = min3(dp[i - 1][j - 1] + pxy ,
-									dp[i - 1][j] + pgap ,
+							dp[i][j] = min(min(dp[i - 1][j - 1] + pxy ,
+									dp[i - 1][j] + pgap) ,
 									dp[i][j - 1] + pgap);
 						}
 					}
@@ -197,48 +197,46 @@ int getMinimumPenalty(std::string x, std::string y, int pxy, int pgap,
 				
 			}
 		}
-		
 	}
 
-    // Reconstructing the solution
-    int l = n + m; // maximum possible length
-
-    i = m;
-    j = n;
-
-    int xpos = l;
-    int ypos = l;
-
-    while ( !(i == 0 || j == 0))
-    {
-        if (x[i - 1] == y[j - 1])
-        {
-            xans[xpos--] = (int)x[i - 1];
-            yans[ypos--] = (int)y[j - 1];
-            i--; j--;
-        }
-        else if (dp[i - 1][j - 1] + pxy == dp[i][j])
-        {
-            xans[xpos--] = (int)x[i - 1];
-            yans[ypos--] = (int)y[j - 1];
-            i--; j--;
-        }
-        else if (dp[i - 1][j] + pgap == dp[i][j])
-        {
-            xans[xpos--] = (int)x[i - 1];
-            yans[ypos--] = (int)'_';
-            i--;
-        }
-        else
-        {
-            xans[xpos--] = (int)'_';
-            yans[ypos--] = (int)y[j - 1];
-            j--;
-        }
-    }
 
 
-    omp_set_num_threads(omp_get_max_threads());
+	// Reconstructing the solution
+	int l = n + m; // maximum possible length
+	
+	i = m; j = n;
+	
+	int xpos = l;
+	int ypos = l;
+	
+	while ( !(i == 0 || j == 0))
+	{
+		if (x[i - 1] == y[j - 1])
+		{
+			xans[xpos--] = (int)x[i - 1];
+			yans[ypos--] = (int)y[j - 1];
+			i--; j--;
+		}
+		else if (dp[i - 1][j - 1] + pxy == dp[i][j])
+		{
+			xans[xpos--] = (int)x[i - 1];
+			yans[ypos--] = (int)y[j - 1];
+			i--; j--;
+		}
+		else if (dp[i - 1][j] + pgap == dp[i][j])
+		{
+			xans[xpos--] = (int)x[i - 1];
+			yans[ypos--] = (int)'_';
+			i--;
+		}
+		else if (dp[i][j - 1] + pgap == dp[i][j])
+		{
+			xans[xpos--] = (int)'_';
+			yans[ypos--] = (int)y[j - 1];
+			j--;
+		}
+	}
+	omp_set_num_threads(omp_get_max_threads());
     int x_diff = xpos - i, y_diff = ypos-j;
 #pragma omp parallel for
     for (int ii = i; ii>0; --ii){
@@ -261,11 +259,12 @@ int getMinimumPenalty(std::string x, std::string y, int pxy, int pgap,
         yans[y_pos2] = (int)'_';
     }
 
-    int ret = dp[m][n];
+	int ret = dp[m][n];
 
-//    delete[] dp[0];
-//    delete[] dp;
-
-    return ret;
+	//delete[] dp[0];
+	//delete[] dp;
+	return ret;
 }
-//g++ -fopenmp -o renjiem-seqalignomp renjiem-seqalignomp.cpp -O3
+
+
+//g++ -fopenmp tianyim-seqalignomp.cpp -o tianyim-seqalignomp -O3
